c1c918a8a6db9e4392da72f045dfc0722a6e9a71
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-01-10 17:45:35 +0300

    topicVC

--- HSK1/ViewControllers/TopicViewController.swift
+++ HSK1/ViewControllers/TopicViewController.swift
@@ -13,14 +13,6 @@ class TopicViewController: UICollectionViewController {
         
     }
 
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
